12ff4181986b6e8977fbcafee9e2d84a41985c7b
Manraj Singh <mn697903@dal.ca>
2021-07-07 20:13:24 -0300

    Adding class ValidateInput single resonsibility and adding other inputs

--- src/main/java/com/dal/cabby/profileManagement/Registration.java
+++ src/main/java/com/dal/cabby/profileManagement/Registration.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Thread.sleep;
+
 public class Registration {
 
     public Registration(){
@@ -12,29 +14,53 @@ public class Registration {
         System.out.print("\nEnter Name : ");
         String name = sc.nextLine();
         String email = "";
+        String password = "";
+        String confirmPassword = "";
+        String phoneNumber = "";
+        String userName = "";
+        ValidateInput validateInput = new ValidateInput();
 
         for(;;){
             System.out.print("\nEnter Email : ");
             email = sc.nextLine();
-            if(validateEmail(email)){
+
+            if(validateInput.validateEmail(email)){
                 break;
             }
             else{
                 System.err.println("\t\tEnter Valid Email!!!!!");
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
-        String userName = sc.next();
+        System.out.print("\nEnter new Username : ");
+        userName = sc.next();
+        System.out.print("\nEnter Password : ");
+        password = sc.nextLine();
+        for(;;){
+            System.out.print("\nConfirm above password : ");
+            confirmPassword = sc.nextLine();
+            if(validateInput.validateConfirmPassword(password, confirmPassword)){
+                break;
+            }
+            else{
+                System.err.println("\t\tConfirm password does't match !!!");
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
         sc.close();
     }
 
-    public boolean validateEmail(String email){
-        String regex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$";
-        Pattern VALIDATE_EMAIL_ADDRESS = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        Matcher emailMatcher = VALIDATE_EMAIL_ADDRESS.matcher(email);
-        return emailMatcher.find();
 
-    }
 
 }
