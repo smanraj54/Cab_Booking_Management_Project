baf3272fe1ba8c493156d8d92cb1995eee8bcbd7
Manjinder <mn758600@dal.ca>
2021-07-08 14:58:06 +0530

    Added skeleton class for price calculation

---
+++ src/main/java/com/dal/cabby/cabPrice/CabPriceCalculator.java
@@ -0,0 +1,9 @@
+package com.dal.cabby.cabPrice;
+
+import java.sql.Time;
+
+public class CabPriceCalculator {
+    public int priceCalculation(String source, String destination, Boolean rideSharing, Time timeOfRide){
+        return 0;
+    }
+}
