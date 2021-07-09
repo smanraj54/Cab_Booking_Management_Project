8617d824621cbc6b397e9f72b3429f5c85dd9689
Bikramjit Singh <bk935353@dal.ca>
2021-07-09 14:42:49 -0300

    Added addDriverRating() method to Ratings class.

--- src/main/java/com/dal/cabby/profiles/Ratings.java
+++ src/main/java/com/dal/cabby/profiles/Ratings.java
@@ -1,5 +1,20 @@
 package com.dal.cabby.profiles;
 
-// This class will responsible for ratings given to the driver and the users.
+import com.dal.cabby.dbHelper.DBHelper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Ratings {
+    DBHelper dbHelper;
+
+    Ratings() {
+        dbHelper = new DBHelper();
+    }
+
+    void addDriverRating(int driverId, int tripId, int rating) throws SQLException {
+        String q = String.format("insert into driver_ratings(driver_id, trip_id, rating) values (%d, %d, %d))",
+                driverId, tripId, rating);
+        dbHelper.executeCreateOrUpdateQuery(q);
+    }
 }
