d1326e5ad6d8c501756178fa62bd7929613153e9
Manraj Singh <mn697903@dal.ca>
2021-07-09 14:59:43 -0300

    DB operations in Registration

--- src/main/java/com/dal/cabby/profileManagement/DB_Operations.java
+++ src/main/java/com/dal/cabby/profileManagement/DB_Operations.java
@@ -1,29 +1,65 @@
 package com.dal.cabby.profileManagement;
 
+import com.dal.cabby.dbHelper.DBHelper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DB_Operations {
+
+    private String query = "Select * from Registration where username = <username>";
 
     public boolean dbUserNameValidation(String userName){
 
-        //DB check for username
-        return true;
+        return dbContainsUserName(userName);
+
     }
 
     public boolean dbContainsUserName(String userName){
 
-        // Validate username exists or not
-        return false;
+        boolean foundUser = false;
+        String value = getValueFromDB(userName, "username");
+        if(value==null){
+            return false;
+        }
+        if(userName.equals(value)){
+           foundUser =  true;
+        }
+        return foundUser;
     }
 
     public String getPassword(String userName){
-        // getPassword form db
-        return "";
+        String password = getValueFromDB(userName, "password");
+
+        return password;
     }
 
     public boolean validatePassword(String userName, String password){
+        String dbPassword = getPassword(userName);
+        if(dbPassword == null){
+            return false;
+        }
+        if(dbPassword.equals(password)){
+            return true;
+        }
+        return false;
+    }
 
-        //return password.equals(getPassword(userName));
-
-        return true;
+    private String getValueFromDB(String userName, String keywordSearch ){
+        String query = this.query.replace("<username>", userName);
+        String value = null;
+        DBHelper dbHelper = new DBHelper();
+        try {
+            dbHelper.initialize();
+            ResultSet resultSet = dbHelper.executeSelectQuery(query);
+            while(resultSet.next()){
+                value = resultSet.getString(keywordSearch);
+            }
+            dbHelper.close();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+        return value;
     }
 
 }
--- src/main/sql/db.sql
+++ src/main/sql/db.sql
@@ -98,5 +98,7 @@ CREATE TABLE IF NOT EXISTS Registration (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL
+
+    );
