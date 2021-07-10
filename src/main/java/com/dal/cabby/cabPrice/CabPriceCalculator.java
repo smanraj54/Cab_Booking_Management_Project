8111551adbb903465107ea5e0c71b4e915855801
Manjinder <mn758600@dal.ca>
2021-07-10 21:36:09 +0530

    Updated file for Price Calculation based on sharing ride with Co-passenger

--- src/main/java/com/dal/cabby/cabPrice/CabPriceCalculator.java
+++ src/main/java/com/dal/cabby/cabPrice/CabPriceCalculator.java
@@ -3,14 +3,16 @@ package com.dal.cabby.cabPrice;
 import java.sql.Time;
 import java.util.Scanner;
 
-
-
 public class CabPriceCalculator {
     static double price=0.0;
     static int hour=java.time.LocalTime.now().getHour();
     static Scanner ss = new Scanner(System.in);
 
-    public static int priceCalculation(String source, String destination, Boolean rideSharing,String sourceArea, Boolean isOfficeHours){
+    public static void main(String[] args) {
+        priceCalculation("Halifax","Montreal",false,"urban",true);
+    }
+
+    public static int priceCalculation(String source, String destination, Boolean rideSharing, String sourceArea, Boolean isOfficeHours){
         System.out.println("*** Select your Preferences ***");
         System.out.println("1. Normal Booking");
         System.out.println("2. Want to share ride with co-passenger");
@@ -19,44 +21,66 @@ public class CabPriceCalculator {
         switch(userInput){
             case 1:
                 distanceFactor(sourceArea,isOfficeHours);
+                System.out.println("Total Price for the ride is: $"+price);
                 break;
             case 2:
+                rideSharing();
                 break;
             case 3:
                 break;
         }
+
         return 0;
     }
 
-    public static double distanceFactor(String rideArea,Boolean isPeakHour) {
-        int totalDistance = 7;
-        int shortDistance = 5;
-        if (totalDistance <= shortDistance) {
-            //For initial 5 kilometers fixed $5 would be charged per Km
-            for (int initialKilometers = 1; initialKilometers < totalDistance; initialKilometers++) {
-                price += 5;
+    public static double distanceFactor(String rideArea,Boolean isPeakHour){
+        int totalDistance=7;
+        int shortDistance=5;
+        if(totalDistance <= shortDistance){
+            //For initial few kilometers 5 dollars would be charged per Km
+            for (int initialKilometers=1; initialKilometers<totalDistance; initialKilometers++){
+                price+=5;
             }
-        } else {
-            for (int lessKilometers = 1; lessKilometers <= shortDistance; lessKilometers++) {
-                price += 5;
+        }
+        else{
+            for(int lessKilometers=1;lessKilometers<=shortDistance;lessKilometers++){
+                price+=5;
             }
-            //Price per kilometer would be reduced for Long Journey after initial 5 kilometers
-            for (int longKilometers = 6; longKilometers <= totalDistance; longKilometers++) {
-                price += 3.5;
+            //Price per kilometer would be reduced for Long Journey
+            for (int longKilometers=6;longKilometers<=totalDistance;longKilometers++ ){
+                price+=3.5;
             }
         }
         // Extra 20% would be charged on base fare if ride timing would be from 9:00 PM till 5:00 AM or During office hours
         if((hour>21 && hour<24)||(hour>01 && hour<05) || isPeakHour){
             price+=(.20 * price);
         }
-
         // rides in urban area would be bit costlier
         if(rideArea=="urban"){
             price+=(.05 * price);
         }
-
-
-        System.out.println("Total Price for the ride is: $" + price);
         return price;
+    }
+
+    public static void rideSharing(){
+        System.out.println("Choose number of co-passengers: ");
+        System.out.println("One co-passenger");
+        System.out.println("Two co-passengers");
+        int input= ss.nextInt();
+        double basicPrice=distanceFactor("urban",true);
+        double priceWithCoPassenger=basicPrice;
+        double discount;
+        switch (input){
+            case 1:
+                priceWithCoPassenger -=(.10*basicPrice);
+                break;
+            case 2:
+                priceWithCoPassenger -=(.15*basicPrice);
+                break;
+        }
+        discount=basicPrice-priceWithCoPassenger;
+        System.out.println("Total Price for this ride is: $"+priceWithCoPassenger);
+        System.out.println("You got a discount of: $"+ String.format("%.2f",discount));
+
     }
 }
