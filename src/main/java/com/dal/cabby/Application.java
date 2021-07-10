a693b7b04fbbe24f56c136477b2c35c32df64880
Manraj Singh <mn697903@dal.ca>
2021-07-10 10:01:28 -0300

    email management

--- src/main/java/com/dal/cabby/Application.java
+++ src/main/java/com/dal/cabby/Application.java
@@ -1,8 +1,6 @@
 package com.dal.cabby;
 
-import com.dal.cabby.prelogin.PreLoginPage;
-import com.dal.cabby.profileManagement.Login;
-import com.dal.cabby.profileManagement.Registration;
+import com.dal.cabby.profileManagement.SendEmail;
 
 // Main starting class
 public class Application {
@@ -10,7 +8,14 @@ public class Application {
 //        PreLoginPage preLoginPage = new PreLoginPage();
 //        preLoginPage.start();
 
-        Login login = new Login();
-        login.attemptLogin();
+//        Login login = new Login();
+//        login.attemptLogin();
+        try{
+            SendEmail.sendEmail("software5408group15@gmail.com",
+                    "Test email",
+                    "<h2>Java Mail Example</h2><p>hi there!</p>");
+        }catch (Exception ee){
+            System.out.println(ee);
+        }
     }
 }
