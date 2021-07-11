0197c3766f81e3e3bb1a4b5d9d95e062ab904608
Arvinder Singh <ar345438@dal.ca>
2021-07-11 23:32:34 +0530

    Added class with skeleton methods to display the rides

---
+++ src/main/java/com/dal/cabby/rides/DisplayRides.java
@@ -0,0 +1,19 @@
+package com.dal.cabby.rides;
+
+public class DisplayRides {
+
+    public DisplayRides() {
+    }
+
+    // method to get daily rides
+    public void getDailyRides() {
+    }
+
+    // method to get monthly rides
+    public void getMonthlyRides() {
+    }
+
+    // method to get rides between specific time period
+    public void getSpecificPeriodRides() {
+    }
+}
