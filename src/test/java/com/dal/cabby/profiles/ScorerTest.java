597f06dfa75a7d032da3614e5e02a27416d741b4
Bikramjit Singh <bk935353@dal.ca>
2021-07-11 14:47:09 -0300

    Added test class for Scorer class

---
+++ src/test/java/com/dal/cabby/profiles/ScorerTest.java
@@ -0,0 +1,18 @@
+package com.dal.cabby.profiles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScorerTest {
+
+    @Test
+    void calculateDriverScore() {
+
+    }
+
+    @Test
+    void calculateCustomerScore() {
+        
+    }
+}
