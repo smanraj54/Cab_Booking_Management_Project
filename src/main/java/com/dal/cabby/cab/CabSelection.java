bb4f40f768aef3fb1c80305f282386aa892bbd91
Manjinder <mn758600@dal.ca>
2021-07-12 01:58:13 +0530

    Updated feature of selecting Cabs from category of Cabs

--- src/main/java/com/dal/cabby/cab/CabSelection.java
+++
@@ -1,9 +0,0 @@
-package com.dal.cabby.cab;
-
-public class CabSelection {
-
-    public String bestPossibleCab(String source, String destination, int driverRatings,String genderPreference){
-        return "cab1";
-    }
-}
-
