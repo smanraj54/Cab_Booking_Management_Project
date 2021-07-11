1a9b92eb2757f0ffc6ce5f02077dd9c8ed6c4835
Arvinder Singh <ar345438@dal.ca>
2021-07-12 01:36:16 +0530

    Added rides page to show and select display options

--- src/main/java/com/dal/cabby/rides/DisplayRides.java
+++ src/main/java/com/dal/cabby/rides/DisplayRides.java
@@ -1,8 +1,42 @@
 package com.dal.cabby.rides;
 
+import java.util.Scanner;
+
 public class DisplayRides {
 
     public DisplayRides() {
+    }
+
+    public void getRides() {
+        ridesPage();
+    }
+
+    private void ridesPage() {
+        while (true) {
+            System.out.println("\n**** Rides Page ****");
+            System.out.println("\t1. Daily rides");
+            System.out.println("\t2. Monthly rides");
+            System.out.println("\t3. Rides between a specific period");
+            System.out.println("\t4. Return to the previous page");
+            System.out.println("Please enter a selection: ");
+            Scanner sc = new Scanner(System.in);
+            int selection = sc.nextInt();
+            switch (selection) {
+                case 1:
+                    getDailyRides();
+                    break;
+                case 2:
+                    getMonthlyRides();
+                    break;
+                case 3:
+                    getSpecificPeriodRides();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("\nInvalid selection");
+            }
+        }
     }
 
     // method to get daily rides
