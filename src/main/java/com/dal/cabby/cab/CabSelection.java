181befe71921e6da29b4b5dc4056b22fe21bc7a7
Manjinder <mn758600@dal.ca>
2021-07-12 01:26:53 +0530

    Revert "Added feature of selecting Cabs from category of Cabs"
    This reverts commit c82c37f19f30152f2c804e92571ea80330874913.

--- src/main/java/com/dal/cabby/cab/CabSelection.java
+++ src/main/java/com/dal/cabby/cab/CabSelection.java
@@ -1,29 +1,9 @@
 package com.dal.cabby.cab;
-import com.dal.cabby.cabPrice.CabPriceCalculator;
-import java.util.Scanner;
 
 public class CabSelection {
-    static Scanner ss=new Scanner(System.in);
-    CabPriceCalculator priceCalculator=new CabPriceCalculator();
 
-    public void preferredCab(){
-        System.out.println("Enter your Cab preference");
-        System.out.println("1. Micro and Mini");
-        System.out.println("2. Prime Sedan");
-        System.out.println("3. Prime SUV");
-        System.out.println("4. Luxury Class");
-        int input=ss.nextInt();
-        switch (input){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
-        //priceCalculator.priceCalculation("Halifax","Montreal",false,"urban",true, input);
+    public String bestPossibleCab(String source, String destination, int driverRatings,String genderPreference){
+        return "cab1";
     }
 }
 
