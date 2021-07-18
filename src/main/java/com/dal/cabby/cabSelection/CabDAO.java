11c86d31e68bb9319437f31b0622205bc3d3c4e6
Manjinder <mn758600@dal.ca>
2021-07-18 23:17:58 +0530

    Created DAO class for Cab Selection feature and added cabSpeedOnRoute attribute in cabs table

---
+++ src/main/java/com/dal/cabby/cabSelection/CabDAO.java
@@ -0,0 +1,64 @@
+package com.dal.cabby.cabSelection;
+
+public class CabDAO {
+    public String cabName;
+    public double cabDistanceFromOrigin;
+    public int driver_Id;
+    public String routeTrafficDensity;
+    public int cabSpeedOnRoute;
+
+    public String toString(){
+        String result=String.format("%s %s %s %s",this.cabName,this.cabDistanceFromOrigin,this.driver_Id,
+                this.routeTrafficDensity,this.cabSpeedOnRoute);
+        return result;
+    }
+
+    public CabDAO(String cabName, double cabDistanceFromOrigin, int driver_Id, String routeTrafficDensity,
+                  int cabSpeedOnRoute){
+        this.cabName=cabName;
+        this.cabDistanceFromOrigin=cabDistanceFromOrigin;
+        this.driver_Id=driver_Id;
+        this.routeTrafficDensity=routeTrafficDensity;
+        this.cabSpeedOnRoute=cabSpeedOnRoute;
+    }
+
+    public String getCabName() {
+        return cabName;
+    }
+
+    public void setCabName(String cabName) {
+        this.cabName = cabName;
+    }
+
+    public double getCabDistanceFromOrigin() {
+        return cabDistanceFromOrigin;
+    }
+
+    public void setCabDistanceFromOrigin(double cabDistanceFromOrigin) {
+        this.cabDistanceFromOrigin = cabDistanceFromOrigin;
+    }
+
+    public int getDriver_Id() {
+        return driver_Id;
+    }
+
+    public void setDriver_Id(int driver_Id) {
+        this.driver_Id = driver_Id;
+    }
+
+    public String getRouteTrafficDensity() {
+        return routeTrafficDensity;
+    }
+
+    public void setRouteTrafficDensity(String routeTrafficDensity) {
+        this.routeTrafficDensity = routeTrafficDensity;
+    }
+
+    public int getCabSpeedOnRoute() {
+        return cabSpeedOnRoute;
+    }
+
+    public void setCabSpeedOnRoute(int cabSpeedOnRoute) {
+        this.cabSpeedOnRoute = cabSpeedOnRoute;
+    }
+}
