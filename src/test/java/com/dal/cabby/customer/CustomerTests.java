837ed55de91411bd4c94eeef0b8d87d7e9433f99
Devraj Singh <dv247596@dal.ca>
2021-07-19 20:10:38 +0530

    Added test for Prelogin and Customer page

--- src/test/java/com/dal/cabby/customer/CustomerTests.java
+++ src/test/java/com/dal/cabby/customer/CustomerTests.java
@@ -1,7 +1,6 @@
-package com.dal.cabby.driver;
+package com.dal.cabby.customer;
 
 import com.dal.cabby.io.PredefinedInputs;
-import com.dal.cabby.util.Constants;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +10,16 @@ import java.util.UUID;
 
 import static com.dal.cabby.util.Constants.*;
 
-public class DriverTests {
-
+public class CustomerTests {
     @Test
     void testExit() {
-        System.out.println("Testing simple exit flow for driver");
+        System.out.println("Testing simple exit flow for customer");
         PredefinedInputs inputs = new PredefinedInputs();
         inputs.add(EXIT);
-        Driver driver;
+        Customer customer;
         try {
-            driver = new Driver(inputs);
-            driver.performTasks();
+            customer = new Customer(inputs);
+            customer.performTasks();
         } catch (SQLException | ParseException throwables) {
             throwables.printStackTrace();
             Assertions.fail(throwables.getMessage());
@@ -30,7 +28,7 @@ public class DriverTests {
 
     @Test
     void testRegistration() {
-        System.out.println("Testing resgitration flow for Driver");
+        System.out.println("Testing resgitration flow for Customer");
         PredefinedInputs inputs = new PredefinedInputs();
         String name = UUID.randomUUID().toString();
         String userName = UUID.randomUUID().toString();
@@ -38,10 +36,10 @@ public class DriverTests {
         String email = String.format("%s@gmail.com", name);
         inputs.add(REGISTRATION).add(name).add(email).add(userName).add(password).add(password).add(EXIT);
 
-        Driver driver;
+        Customer customer;
         try {
-            driver = new Driver(inputs);
-            driver.performTasks();
+            customer = new Customer(inputs);
+            customer.performTasks();
         } catch (SQLException | ParseException throwables) {
             throwables.printStackTrace();
             Assertions.fail(throwables.getMessage());
@@ -50,7 +48,7 @@ public class DriverTests {
 
     @Test
     void testLogin() {
-        System.out.println("Testing login flow for Driver");
+        System.out.println("Testing login flow for Customer");
         PredefinedInputs inputs = new PredefinedInputs();
         String name = UUID.randomUUID().toString();
         String userName = UUID.randomUUID().toString();
@@ -59,10 +57,10 @@ public class DriverTests {
         inputs.add(REGISTRATION).add(name).add(email).add(userName).add(password)
                 .add(password).add(LOGIN).add(userName).add(password).add(LOGOUT).add("y").add(EXIT);
 
-        Driver driver;
+        Customer customer;
         try {
-            driver = new Driver(inputs);
-            driver.performTasks();
+            customer = new Customer(inputs);
+            customer.performTasks();
         } catch (SQLException | ParseException throwables) {
             throwables.printStackTrace();
             Assertions.fail(throwables.getMessage());
