06151ea6fa2ed767271d0226975daa6f0ce568ad
Bikramjit Singh <bk935353@dal.ca>
2021-07-22 12:20:02 -0300

    giveCustomerBonus() method implemented.

--- src/main/java/com/dal/cabby/incentives/CustomerBonus.java
+++ src/main/java/com/dal/cabby/incentives/CustomerBonus.java
@@ -2,8 +2,16 @@ package com.dal.cabby.incentives;
 
 public class CustomerBonus {
 
-    public int giveCustomerBonus(int rating){
-        return 0;
+    public int giveCustomerBonus(int rating) {
+        if (rating == 5) {
+            System.out.println("Congratulations! You are eligible for a 10% cashback on your last trip.");
+            return 10;
+        } else if (rating == 4) {
+            System.out.println("Congratulations! You are eligible for a 5% cashback on your last trip.");
+            return 5;
+        } else {
+            System.out.println("Trip Ended Successfully.");
+            return 0;
+        }
     }
-
 }
