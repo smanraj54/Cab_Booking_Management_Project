42319b38b9e55aa02faf7eb31cf9703c61ac0526
Bikramjit Singh <bk935353@dal.ca>
2021-07-22 12:07:17 -0300

    DriverBonus class test cases added, TDD being followed.

---
+++ src/test/java/com/dal/cabby/incentives/DriverBonusTest.java
@@ -0,0 +1,23 @@
+package com.dal.cabby.incentives;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DriverBonusTest {
+
+    @Test
+    void giveDriverBonusFiveStars() {
+        DriverBonus driverBonus = new DriverBonus();
+        int bonus = driverBonus.giveDriverBonus(5);
+        Assertions.assertEquals(2, bonus, "Incentive not correct.");
+    }
+
+    @Test
+    void giveDriverBonusFourStars() {
+        DriverBonus driverBonus = new DriverBonus();
+        int bonus = driverBonus.giveDriverBonus(4);
+        Assertions.assertEquals(1, bonus, "Incentive not correct.");
+    }
+}
