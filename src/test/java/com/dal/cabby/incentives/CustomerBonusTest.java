1fd8ad28ee25d6b63477199da0999c767821ecd1
Bikramjit Singh <bk935353@dal.ca>
2021-07-22 12:15:16 -0300

    CustomerBonus class unit test cases added and failed!, TDD being followed.

---
+++ src/test/java/com/dal/cabby/incentives/CustomerBonusTest.java
@@ -0,0 +1,24 @@
+package com.dal.cabby.incentives;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerBonusTest {
+
+    @Test
+    void giveCustomerBonusFiveStars() {
+        CustomerBonus customerBonus = new CustomerBonus();
+        int bonus = customerBonus.giveCustomerBonus(5);
+        Assertions.assertEquals(10, bonus, "Cashback not correct.");
+    }
+
+    @Test
+    void giveCustomerBonusFourStars() {
+        CustomerBonus customerBonus = new CustomerBonus();
+        int bonus = customerBonus.giveCustomerBonus(4);
+        Assertions.assertEquals(5, bonus, "Cashback not correct.");
+    }
+
+}
