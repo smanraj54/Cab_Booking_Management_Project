35c71ead2cbf781baabc7fcc0c90c7973e1ec49c
Manjinder <mn758600@dal.ca>
2021-07-23 02:45:02 +0530

    Added more test cases for Micro and Mini cab category while selecting co-passenger and aadditional amenities during ride

--- src/test/java/com/dal/cabby/cabSelection/CabSelectionTests.java
+++ src/test/java/com/dal/cabby/cabSelection/CabSelectionTests.java
@@ -10,13 +10,32 @@ import java.sql.SQLException;
 
 public class CabSelectionTests {
     @Test
-    void microAndMiniCategoryTest() throws SQLException {
+    void microAndMiniNormalBookingTest() throws SQLException {
         PredefinedInputs inputs=new PredefinedInputs();
         inputs.add(1).add("Halifax").add("Dartmouth").add(1).add(2).add(1);
-        CabSelectionService cabSelectionService=new CabSelectionService(inputs);
+        CabSelectionService cabSelectionService = new CabSelectionService(inputs);
         Booking booking=cabSelectionService.preferredCab(1,20);
-        double expectedPrice=84.5;
-        //double actualPrice=cabSelectionService.preferredCab();
+        double expectedPrice=88.725;
+        Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
+    }
+
+    @Test
+    void microAndMiniRideSharingTest() throws SQLException {
+        PredefinedInputs inputs = new PredefinedInputs();
+        inputs.add(1).add("Halifax").add("Sydney").add(1).add(1).add(2).add(1);
+        CabSelectionService cabSelectionService = new CabSelectionService(inputs);
+        Booking booking=cabSelectionService.preferredCab(1,20);
+        double expectedPrice=63.315;
+        Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
+    }
+
+    @Test
+    void microAndMiniWithAmenitiesTest() throws SQLException {
+        PredefinedInputs inputs = new PredefinedInputs();
+        inputs.add(1).add("Halifax").add("Toronto").add(1).add(2).add(3).add(2);
+        CabSelectionService cabSelectionService = new CabSelectionService(inputs);
+        Booking booking=cabSelectionService.preferredCab(1,20);
+        double expectedPrice=441.22999999999996;
         Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
     }
 
