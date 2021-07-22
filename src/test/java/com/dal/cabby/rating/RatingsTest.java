0ad80be5045090c17cf0fb47e11e3d70139600bc
Bikramjit Singh <bk935353@dal.ca>
2021-07-22 11:37:41 -0300

    added test case for an invalid driver rating.

--- src/test/java/com/dal/cabby/rating/RatingsTest.java
+++ src/test/java/com/dal/cabby/rating/RatingsTest.java
@@ -1,5 +1,6 @@
 package com.dal.cabby.rating;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -26,6 +27,17 @@ class RatingsTest {
         ratings.addCustomerRating(driverId, tripId, rating);
     }
 
+    @Test
+    void addInvalidDriverRating() throws SQLException {
+        IRatings ratings = new Ratings();
+        int driverId = 1;
+        int customerId = 1;
+        int rating = 7;
+        // Expect a runtime exception because valid rating is between 1 to 5
+        Assertions.assertThrows(RuntimeException.class, () ->ratings.addDriverRating(driverId, customerId, rating));
+    }
+
+
 
     @Test
     void getAverageRatingOfDriver() {
@@ -35,4 +47,6 @@ class RatingsTest {
     void getAverageRatingOfCustomer() {
 
     }
+
+
 }
