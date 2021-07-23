cd460ab658d9900f0918a433fc09c6cf7ef92972
Manjinder <mn758600@dal.ca>
2021-07-23 18:55:52 +0530

    Test case for Prime Sedan(With Amenities) and Prime SUV (Normal booking) categories

--- src/test/java/com/dal/cabby/cabSelection/CabSelectionTests.java
+++ src/test/java/com/dal/cabby/cabSelection/CabSelectionTests.java
@@ -45,10 +45,12 @@ public class CabSelectionTests {
         inputs.add(2).add("Sydney").add("Dartmouth").add(1).add(2).add(1);
         CabSelectionService cabSelectionService = new CabSelectionService(inputs);
         Booking booking=cabSelectionService.preferredCab(1,20);
-        // Total distance = 39KM (Price for distance= 144)
-        // 10% extra charge for Prime Sedan Cab type  => (10% of 144)= 14.4
-        // 5% extra charge for Urban area as Sydney comes in Urban area
-        // Ride is not during office hours and Source area is also Rural so no extra charge for these parameters
+        /*
+          a. Total distance = 39KM (Price for distance= 144)
+          b. 10% extra charge for Prime Sedan Cab type  => (10% of 144)= 14.4
+          c. 5% extra charge for Urban area as Sydney comes in Urban area
+          d. Ride is not during office hours so no extra charge for this parameters
+         */
         double expectedPrice=166.32;
         Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
     }
@@ -59,12 +61,43 @@ public class CabSelectionTests {
         inputs.add(2).add("Dartmouth").add("Toronto").add(1).add(2).add(2).add(1);
         CabSelectionService cabSelectionService = new CabSelectionService(inputs);
         Booking booking=cabSelectionService.preferredCab(1,20);
-        // Total distance = 91KM (Price for distance= 326)
-        // Cab type is prime Sedan so 10% extra charge => (10% of 326)= 32.6
-        // Ride is not during office hours and Source area is also Rural so no extra charge for these parameters
-        // 10% of discount on total amount, upon sharing ride with co passenger
+        /*
+          a. Total distance = 91KM (Price for distance= 326)
+          b. Cab type is prime Sedan so 10% extra charge => (10% of 326)= 32.6
+          c. Ride is not during office hours and Source area is also Rural so no extra charge for these parameters
+          d. 10% of discount on total amount, upon sharing ride with co passenger
+         */
         double expectedPrice=322.74;
         Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
     }
 
+    @Test
+    void primeSedanWithAmenitiesTest() throws SQLException {
+        PredefinedInputs inputs = new PredefinedInputs();
+        inputs.add(2).add("Yarmouth").add("Halifax").add(1).add(1).add(3).add(3);
+        CabSelectionService cabSelectionService = new CabSelectionService(inputs);
+        Booking booking=cabSelectionService.preferredCab(1,20);
+        /* a. Total distance = 12KM (Price for distance= 49.5)
+           b. Cab type is prime Sedan so 10% extra charge => (10% of 49.5)= 4.95
+           c. Ride is not during office hours and Source area is also Rural so no extra charge for these parameters
+           d. Extra charge for availing both amenities = 3.5555555555555554
+        */
+        double expectedPrice=58.00555555555556;
+        Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
+    }
+
+    @Test
+    void primeSUVNormalBookingTest() throws SQLException {
+        PredefinedInputs inputs = new PredefinedInputs();
+        inputs.add(3).add("Toronto").add("Halifax").add(1).add(1).add(1);
+        CabSelectionService cabSelectionService = new CabSelectionService(inputs);
+        Booking booking=cabSelectionService.preferredCab(1,20);
+        /* a. Total distance = 113KM (Price for distance= 403)
+           b. Cab type is prime SUV so 25% extra charge => (25% of 403 )= 100.75
+           c. 5% extra charge for Urban area as Toronto comes in Urban area
+           d. Ride is not during office hours so no extra charge for this parameters
+        */
+        double expectedPrice=528.9375;
+        Assertions.assertEquals(expectedPrice,booking.getPrice(),"Error in calculating right price");
+    }
 }
