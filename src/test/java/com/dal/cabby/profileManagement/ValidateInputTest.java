f60ef1f27897b9e5081d343a5e2bf4f0609f418c
Manraj Singh <mn697903@dal.ca>
2021-07-23 15:52:03 -0300

    LoggedInProfile details class testcases added

--- src/test/java/com/dal/cabby/profileManagement/ValidateInputTest.java
+++ src/test/java/com/dal/cabby/profileManagement/ValidateInputTest.java
@@ -2,12 +2,11 @@ package com.dal.cabby.profileManagement;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ValidateInputTest {
 
     @Test
     void validateEmail() {
+
     }
 
     @Test
