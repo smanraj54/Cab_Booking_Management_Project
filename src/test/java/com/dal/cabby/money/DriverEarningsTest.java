8061f12c8d45b0299b7cfd1cae7e15fb3b809c22
Arvinder Singh <ar345438@dal.ca>
2021-07-24 13:11:24 +0530

    Added test case for Driver's earnings between specific period

--- src/test/java/com/dal/cabby/money/DriverEarningsTest.java
+++ src/test/java/com/dal/cabby/money/DriverEarningsTest.java
@@ -90,8 +90,38 @@ public class DriverEarningsTest {
 
     @Test
     public void testSpecificPeriodEarnings() throws SQLException {
-        Inputs inputs = new PredefinedInputs();
+
+        // cleaning data for testing
+        iPersistence.executeCreateOrUpdateQuery("delete from trips where trip_id in (201, 202, 203, 204);");
+        iPersistence.executeCreateOrUpdateQuery("delete from bookings where booking_id in (201, 202, 203, 204);");
+
+        // inserting data for testing in booking table
+        iPersistence.executeCreateOrUpdateQuery("insert into bookings(booking_id, " +
+            "created_at, driver_id, cust_id, cab_id, estimated_price, source, destination) values " +
+            "(201, '2010-01-01 20:00:00', 1, 1, 1, 150, 'Halifax', 'Toronto'), " +
+            "(202, '2010-01-01 20:00:00', 1, 2, 1, 150, 'Winnipeg', 'Toronto'), " +
+            "(203, '2010-12-31 20:00:00', 1, 3, 1, 150, 'Halifax', 'Toronto'), " +
+            "(204, '2010-12-31 20:00:00', 1, 4, 1, 150, 'Winnipeg', 'Toronto');");
+
+        // inserting data for testing in trips table
+        iPersistence.executeCreateOrUpdateQuery("insert into trips(trip_id, driver_id, cust_id, " +
+            "booking_id, trip_amount, distance_covered, created_at) values " +
+            "(201, 1, 1, 201, 150, 1600, '2010-01-01 20:00:00'), " +
+            "(202, 1, 2, 202, 80.5, 200, '2010-01-01 20:00:00'), " +
+            "(203, 1, 3, 203, 150, 1600, '2010-12-31 20:00:00'), " +
+            "(204, 1, 4, 204, 80.5, 200, '2010-12-31 20:00:00');");
+
+        PredefinedInputs inputs = new PredefinedInputs();
+        inputs.add(3).add("01/01/2010").add("31/12/2010");
         DriverEarnings driverEarnings = new DriverEarnings(inputs);
-        assertEquals(10000, 10000, "Earning is not correct for given period");
+
+        /*
+         * Earning will be (150 + 80.5 = 230.5) * 2
+         * 230.5 - commission (15% of total - distance is greater than 300)
+         * (230.5 - 34.575) * 2 = 195.925 * 2 = 391.85
+         */
+        assertEquals("\nTotal earnings between 01/01/2010 and 31/12/2010 is $391.85",
+            driverEarnings.getEarnings(1), "Driver's earning is not " +
+                "calculated correctly");
     }
 }
