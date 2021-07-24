156ca1160b3f4a3c84c4fd4776603d5a0762cf37
Manjinder <mn758600@dal.ca>
2021-07-24 18:11:59 +0530

    SOLID Principle implemented. Create new class for Calculating price with amenities to overcome Violation of Single responsibility principle

--- src/main/java/com/dal/cabby/cabSelection/CabSelectionDAO.java
+++ src/main/java/com/dal/cabby/cabSelection/CabSelectionDAO.java
@@ -9,6 +9,12 @@ public class CabSelectionDAO {
     public int cabSpeedOnRoute;
     public String driverGender;
 
+    public String toString() {
+        String result = String.format("%s -> Driver_ID: %s -> Gender: %s ", this.cabName,
+                this.driver_Id, this.driverGender);
+        return result;
+    }
+
     public CabSelectionDAO(String cabName, int cabId, double cabDistanceFromOrigin, int driver_Id,
                            String routeTrafficDensity, int cabSpeedOnRoute, String driverGender) {
         this.cabName = cabName;
@@ -29,19 +35,15 @@ public class CabSelectionDAO {
     public String getCabName() {
         return cabName;
     }
-
     public void setCabName(String cabName) {
         this.cabName = cabName;
     }
-
     public double getCabDistanceFromOrigin() {
         return cabDistanceFromOrigin;
     }
-
     public void setCabDistanceFromOrigin(double cabDistanceFromOrigin) {
         this.cabDistanceFromOrigin = cabDistanceFromOrigin;
     }
-
     public int getDriver_Id() {
         return driver_Id;
     }
