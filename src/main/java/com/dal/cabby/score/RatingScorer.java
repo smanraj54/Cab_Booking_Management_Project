d6cc19257094a831129881c0a93981a8b5c1a8e7
Arvinder Singh <ar345438@dal.ca>
2021-07-24 13:57:19 +0530

    removing unused imports as a part of refactoring

--- src/main/java/com/dal/cabby/score/RatingScorer.java
+++ src/main/java/com/dal/cabby/score/RatingScorer.java
@@ -1,6 +1,5 @@
 package com.dal.cabby.score;
 
-
 // This class wll work on the scores of driver and customers based on rating and ride performance.
 public class RatingScorer implements IRatingScorer {
 
