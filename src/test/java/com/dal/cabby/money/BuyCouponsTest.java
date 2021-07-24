71b37201e356f3f339ed5734df3571640426a785
Arvinder Singh <ar345438@dal.ca>
2021-07-24 10:17:17 +0530

    added test case to check when user don't have sufficient points to buy coupon

--- src/test/java/com/dal/cabby/money/BuyCouponsTest.java
+++ src/test/java/com/dal/cabby/money/BuyCouponsTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BuyCouponsTest {
 
   @Test
-  void test() throws SQLException {
+  void testBuyCoupon() throws SQLException {
     IPersistence persistence = DBHelper.getInstance();
     persistence.executeCreateOrUpdateQuery("update user_points set total_points = 100 " +
         "where user_id = 1 and user_type = 'CUSTOMER';");
@@ -21,6 +21,19 @@ public class BuyCouponsTest {
     inputs.add("y").add(101);
     BuyCoupons buyCoupons = new BuyCoupons(inputs);
     assertEquals("\nCoupon added in your account successfully",
+        buyCoupons.getCoupons(1, UserType.CUSTOMER),
+        "Coupon is not added");
+  }
+
+  @Test
+  void testBuyCouponLessPoints() throws SQLException {
+    IPersistence persistence = DBHelper.getInstance();
+    persistence.executeCreateOrUpdateQuery("update user_points set total_points = 100 " +
+        "where user_id = 1 and user_type = 'CUSTOMER';");
+    PredefinedInputs inputs = new PredefinedInputs();
+    inputs.add("y").add(103);
+    BuyCoupons buyCoupons = new BuyCoupons(inputs);
+    assertEquals("\nYou don't have sufficient points to buy this coupon",
         buyCoupons.getCoupons(1, UserType.CUSTOMER),
         "Coupon is not added");
   }
