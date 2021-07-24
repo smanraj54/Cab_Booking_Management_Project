850031e6d13c9167e7b4173a9ef92b072205f897
Manjinder <mn758600@dal.ca>
2021-07-24 15:47:37 +0530

    SOLID principle implemented. Implemented Single Responsibility Principle in cabPrice package

--- src/main/java/com/dal/cabby/cabPrice/CabPriceCalculator.java
+++ src/main/java/com/dal/cabby/cabPrice/CabPriceCalculator.java
@@ -9,15 +9,20 @@ import com.dal.cabby.io.Inputs;
 public class CabPriceCalculator implements ICabPriceCalculator{
     IPersistence iPersistence;
     Inputs inputs;
+    double distance=0.0;
+    CabPriceDistanceFactor cabPriceDistanceFactor;
+    CabPriceRideSharing cabPriceRideSharing;
+    //CabPriceAmenities cabPriceAmenities;
     public CabPriceCalculator(Inputs inputs){
         this.inputs=inputs;
+        cabPriceDistanceFactor =new CabPriceDistanceFactor(inputs);
+        cabPriceRideSharing=new CabPriceRideSharing(inputs);
         try {
             iPersistence = DBHelper.getInstance();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-    double distance=0.0;
 
     @Override
     public double priceCalculation(String source, String destination, int cabType, double hour) throws SQLException {
@@ -29,11 +34,11 @@ public class CabPriceCalculator implements ICabPriceCalculator{
         distance=locationsDistanceFromOrigin(source,destination);
         switch(userInput){
             case 1:
-                return distanceFactor(source,distance,cabType,hour);
+                return cabPriceDistanceFactor.distanceFactor(source,distance,cabType,hour);
             case 2:
-                return rideSharing(source,distance,cabType,hour);
+                return cabPriceRideSharing.rideSharing(source,distance,cabType,hour);
             case 3:
-                return amenities(source,distance,cabType,hour);
+                //return cabPriceAmenities.amenities(source,distance,cabType,hour);
             case 4:
                 System.out.println("Invalid option selected");
         }
@@ -103,113 +108,47 @@ public class CabPriceCalculator implements ICabPriceCalculator{
         }
         return (Math.round(distance*100.0)/100.0);
     }
-    private double distanceFactor(String source,double distance,int cabType, double hour) throws SQLException {
-        double shortDistance = 5; //For initial few kilometers 5 dollars would be charged per Km
-        String rideArea = null;
-        double price=0.0;
-        if(distance <= shortDistance){
-            for(int initialKilometers=1; initialKilometers<=distance; initialKilometers++){
-                price += 5;
-            }
-        }
-        else{
-            for(int lessKilometers=1;lessKilometers<=shortDistance;lessKilometers++){
-                price += 5;
-            }
-            //Price per kilometer would be reduced for Long Journey
-            for (int longKilometers=6;longKilometers<=distance;longKilometers++ ){
-                price += 3.5;
-            }
-        }
-        // Extra 20% would be charged on base fare if ride timing would be from 9:00 PM till 5:00 AM or During office hours
-        if((hour>=21 && hour<=24)||(hour>=00 && hour<05) || (hour>=17 && hour<19)){
-            price += (.20 * price);
-        }
-        // rides in urban area would be bit costlier
-        String query = String.format("Select sourceArea from price_Calculation where sourceName='%s'",source);
-        ResultSet resultSet = iPersistence.executeSelectQuery(query);
-        while (resultSet.next()){
-            rideArea = resultSet.getString("sourceArea");
-        }
-        if(rideArea.equals("urban")){
-            price+=(.05 * price);
-        }
 
-        if(cabType==2) {
-            price+=(.1*price);  //10% price would be higher for Prime Sedan category of Cabs
-        }
-        if(cabType==3){
-            price+=(.25*price);  //25% price would be higher for Prime SUV category of Cabs
-        }
-        if(cabType==4) {
-            price += (.40 * price);  //40% price would be higher for PLuxury Class Cabs
-        }
-        return (Math.round(price*100.0)/100.0);
-    }
-
-    private double rideSharing(String source, double distance, int cabCategory, double hour) throws SQLException {
-        System.out.println("Choose number of co-passengers: ");
-        System.out.println("One co-passenger");
-        System.out.println("Two co-passengers");
-        int input= inputs.getIntegerInput();
-        double basicPrice=distanceFactor(source,distance,cabCategory,hour);
-        System.out.println("Price without Co-passenger: $"+String.format("%.2f",basicPrice));
-        double priceWithCoPassenger=basicPrice;
-        double discount;
-        switch (input){
-            case 1:
-                priceWithCoPassenger -=(.10*basicPrice);
-                break;
-            case 2:
-                priceWithCoPassenger -=(.15*basicPrice);
-                break;
-        }
-        discount=basicPrice-priceWithCoPassenger;
-        System.out.println("You got a discount of $"+ String.format("%.2f",discount)+" on sharing ride with co-passenger");
-        System.out.println("Total Price for this ride is: $"+ String.format("%.2f",priceWithCoPassenger));
-        return (Math.round(priceWithCoPassenger*100.0)/100.0);
-    }
-
-    private double amenities(String source,double distance, int cabCategory, double hour) throws SQLException {
-        // For every 30 minutes of ride we are charging extra $2 per amenity.
-        System.out.println("Choose amenities:");
-        System.out.println("1. CarTV");
-        System.out.println("2. Wifi");
-        System.out.println("3. Both");
-        int input= inputs.getIntegerInput();
-        double basicPrice= distanceFactor(source,distance, cabCategory,hour);
-        System.out.println("Price without amenities: $"+String.format("%.2f",basicPrice));
-        double priceWithAmenities= basicPrice;
-        double extraCharge=0;
-        double speed=0.0;
-        String query=String.format("Select averageSpeed from price_Calculation where sourceName='%s'",source);
-        ResultSet resultSet= iPersistence.executeSelectQuery(query);
-        while(resultSet.next()){
-            speed=resultSet.getDouble("averageSpeed");
-        }
-
-        double time=(distance/speed)*60;  //Converted hours into minutes
-        double rideInMinutes=(time/30);
-        switch (input){
-            case 1:
-                extraCharge=(2*rideInMinutes);
-                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
-                priceWithAmenities+=extraCharge;
-                break;
-            case 2:
-                extraCharge=(2*rideInMinutes);
-                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
-                priceWithAmenities+=extraCharge;
-                break;
-            case 3:
-                extraCharge=(2*(2*rideInMinutes));
-                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
-                priceWithAmenities+=extraCharge;
-                break;
-        }
-        System.out.println("Total Price for this ride is: $"+ String.format("%.2f",priceWithAmenities));
-        return (Math.round(priceWithAmenities*100.0)/100.0);
-    }
+//    private double amenities(String source,double distance, int cabCategory, double hour) throws SQLException {
+//        // For every 30 minutes of ride we are charging extra $2 per amenity.
+//        System.out.println("Choose amenities:");
+//        System.out.println("1. CarTV");
+//        System.out.println("2. Wifi");
+//        System.out.println("3. Both");
+//        int input= inputs.getIntegerInput();
+//        double basicPrice= distanceFactor(source,distance, cabCategory,hour);
+//        System.out.println("Price without amenities: $"+String.format("%.2f",basicPrice));
+//        double priceWithAmenities= basicPrice;
+//        double extraCharge=0;
+//        double speed=0.0;
+//        String query=String.format("Select averageSpeed from price_Calculation where sourceName='%s'",source);
+//        ResultSet resultSet= iPersistence.executeSelectQuery(query);
+//        while(resultSet.next()){
+//            speed=resultSet.getDouble("averageSpeed");
+//        }
+//
+//        double time=(distance/speed)*60;  //Converted hours into minutes
+//        double rideInMinutes=(time/30);
+//        switch (input){
+//            case 1:
+//                extraCharge=(2*rideInMinutes);
+//                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
+//                priceWithAmenities+=extraCharge;
+//                break;
+//            case 2:
+//                extraCharge=(2*rideInMinutes);
+//                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
+//                priceWithAmenities+=extraCharge;
+//                break;
+//            case 3:
+//                extraCharge=(2*(2*rideInMinutes));
+//                System.out.println("Extra charges: $"+String.format("%.2f",extraCharge));
+//                priceWithAmenities+=extraCharge;
+//                break;
+//        }
+//        System.out.println("Total Price for this ride is: $"+ String.format("%.2f",priceWithAmenities));
+//        return (Math.round(priceWithAmenities*100.0)/100.0);
+//    }
 
     public static void main(String[] args) {
         double num=2.45678;
